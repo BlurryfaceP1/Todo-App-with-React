a4603b1d2402a7c1eb24636a894ef9231bb5c71a
Prathmesh <prathmeshwavhal@gmail.com>
2021-07-31 23:54:34 +0530

    first commit

---
+++ build/precache-manifest.4ce460d97d85c367f5b9fa691485dbbd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7def63a72316d1328d876d80b476c7d6",
+    "url": "/Todo-App-with-React/index.html"
+  },
+  {
+    "revision": "f6b62849046510b28243",
+    "url": "/Todo-App-with-React/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "94084334e2aefd1ae117",
+    "url": "/Todo-App-with-React/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "f6b62849046510b28243",
+    "url": "/Todo-App-with-React/static/js/2.71e4e46a.chunk.js"
+  },
+  {
+    "revision": "94084334e2aefd1ae117",
+    "url": "/Todo-App-with-React/static/js/main.81f505a1.chunk.js"
+  },
+  {
+    "revision": "612b405669ddefa6859a",
+    "url": "/Todo-App-with-React/static/js/runtime~main.885e9f7f.js"
+  }
+]);
